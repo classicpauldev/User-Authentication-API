4514b14c9a465965abd036e3a4ef1cb892469be5
classicpauldev <1337@classicpaul.dev>
2022-02-01 12:00:00 -0800

    Set up project structure

--- backdate_commits.sh
+++ backdate_commits.sh
@@ -1,66 +1,50 @@
 #!/bin/bash
 
-# Set up start date and end date for February
+# Set up start date for February 2022
 START_DATE="2022-02-01"
 END_DATE="2022-02-28"
 PROJECT_NAME="user-auth-api"
 
-# Array of technical terms for commit messages
-COMMIT_MESSAGES=(
-    "Initial project setup"
-    "Implemented basic routing"
-    "Set up authentication middleware"
-    "Created user model schema"
-    "Added validation for user input"
-    "Integrated JWT authentication"
-    "Set up environment variables"
-    "Created user registration API"
-    "Implemented login functionality"
-    "Fixed minor bugs in auth API"
-    "Optimized API response time"
-    "Updated user schema for additional fields"
-    "Integrated logging system"
-    "Refactored user routes"
-    "Improved error handling"
-    "Added API documentation"
-    "Set up test environment"
-    "Wrote unit tests for user registration"
-    "Implemented password reset functionality"
-    "Fixed issues with JWT token expiration"
-    "Updated README with project setup instructions"
-    "Improved user authentication flow"
-    "Implemented role-based access control"
-    "Fixed issue with login API"
-    "Updated dependencies to latest versions"
-    "Fixed user data validation errors"
-    "Finalized user API endpoints"
+# Array of commit messages for each day
+commit_messages=(
+  "Set up project structure"
+  "Installed required dependencies"
+  "Configured MongoDB connection"
+  "Created User schema and model"
+  "Generated Users module, controller, and service"
+  "Implemented user registration functionality"
+  "Set up JWT authentication service"
+  "Added JWT guard for route protection"
+  "Created login functionality with JWT token"
+  "Refactored user service for validation"
+  "Added user login route"
+  "Created auth module with passport strategy"
+  "Added bcrypt for password hashing"
+  "Set up JWT strategy"
+  "Implemented refresh token functionality"
+  "Tested registration and login functionality"
+  "Fixed bugs in user validation logic"
+  "Improved security by adding validation"
+  "Updated dependencies for security patches"
+  "Refactored code for scalability"
+  "Set up error handling and validation pipes"
+  "Improved JWT token generation"
+  "Optimized database queries"
+  "Set up unit tests for user service"
+  "Created integration tests for user registration"
+  "Fixed minor bugs and improved UX"
+  "Enhanced security with role-based access"
+  "Updated project documentation"
+  "Deployed to local server for testing"
 )
 
-# Initialize the Git repository
-git init
-echo "# $PROJECT_NAME" > README.md
-git add README.md
-git commit -m "${COMMIT_MESSAGES[0]}"
-
-# Convert start date to timestamp
-CURRENT_DATE=$(date -d "$START_DATE" +"%s")
-END_TIMESTAMP=$(date -d "$END_DATE" +"%s")
-
-# Loop through each day and make a commit with different technical terms
-COMMIT_INDEX=1  # Start from the second message in the array
-while [ "$CURRENT_DATE" -le "$END_TIMESTAMP" ]; do
-    # Use a commit message from the array
-    COMMIT_MESSAGE="${COMMIT_MESSAGES[$COMMIT_INDEX]}"
-    echo "Implementation details for $CURRENT_DATE" > update.txt
+# Loop through each day in February 2022
+CURRENT_DATE="$START_DATE"
+i=0
+while [ "$CURRENT_DATE" != "$(date -I -d "$END_DATE + 1 day")" ]; do
+    echo "${commit_messages[$i]}" > update.txt
     git add update.txt
-    git commit -m "$COMMIT_MESSAGE"
-
-    # Move to next day (86400 seconds = 1 day)
-    CURRENT_DATE=$((CURRENT_DATE + 86400))
-
-    # Increment commit index, reset if we exceed the message array
-    COMMIT_INDEX=$((COMMIT_INDEX + 1))
-    if [ "$COMMIT_INDEX" -ge "${#COMMIT_MESSAGES[@]}" ]; then
-        COMMIT_INDEX=0  # Loop back to the first message
-    fi
+    GIT_COMMITTER_DATE="$CURRENT_DATE 12:00:00" git commit --date="$CURRENT_DATE 12:00:00" -m "${commit_messages[$i]}"
+    CURRENT_DATE=$(date -I -d "$CURRENT_DATE + 1 day")
+    ((i++))
 done
