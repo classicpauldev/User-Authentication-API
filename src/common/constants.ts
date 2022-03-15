c713f8fe207530c5f3b364bf3af636cfbf64b9d4
classicpauldev <1337@classicpaul.dev>
2022-03-15 20:30:28 +0100

    Add ERROR_INVALID_CREDENTIALS constant

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,3 +1,4 @@
 export const JWT_EXPIRY = '1d';
 export const BCRYPT_ROUNDS = 10;
 export const ERROR_EMAIL_EXISTS = 'Email already registered';
+export const ERROR_INVALID_CREDENTIALS = 'Invalid credentials';
