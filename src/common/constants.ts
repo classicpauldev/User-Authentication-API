a027c5cd83520f15f99c80a01c2f6b2c0412dac9
classicpauldev <1337@classicpaul.dev>
2022-03-16 20:30:28 +0100

    Add ERROR_USER_NOT_FOUND constant

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -2,3 +2,4 @@ export const JWT_EXPIRY = '1d';
 export const BCRYPT_ROUNDS = 10;
 export const ERROR_EMAIL_EXISTS = 'Email already registered';
 export const ERROR_INVALID_CREDENTIALS = 'Invalid credentials';
+export const ERROR_USER_NOT_FOUND = 'User not found';
