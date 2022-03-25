f027e77b1d677cd75eb49c604275fa5aeb18d20a
classicpauldev <1337@classicpaul.dev>
2022-03-25 09:00:00 +0100

    Improve src/health/health.controller.ts

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 
+/** Health check endpoint for load balancers and monitoring. */
 @Controller('health')
 export class HealthController {
   @Get()
