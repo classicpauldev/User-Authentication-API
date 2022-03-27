cf0ef2879161fbb5ae9f597ce8910f36df9493be
classicpauldev <1337@classicpaul.dev>
2022-03-27 09:00:00 +0100

    Improve src/auth/guards/local-auth.guard.ts

--- src/auth/guards/local-auth.guard.ts
+++ src/auth/guards/local-auth.guard.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
+/** Guard for local (email/password) authentication. */
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {}
