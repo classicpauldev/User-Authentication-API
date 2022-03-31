5aebf36a9f4627a21e104f38aafae9c79f6b44c5
classicpauldev <1337@classicpaul.dev>
2022-03-31 09:00:00 +0100

    Improve src/auth/decorators/current-user.decorator.ts

--- src/auth/decorators/current-user.decorator.ts
+++ src/auth/decorators/current-user.decorator.ts
@@ -1,5 +1,6 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
+/** Extracts the current authenticated user from the request. */
 export const CurrentUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
