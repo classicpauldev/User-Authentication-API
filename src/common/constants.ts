be1927a1e86495598a69f3466b90ee6a103857fb
classicpauldev <1337@classicpaul.dev>
2022-05-01 09:00:00 +0100

    Add PASSWORD_MIN_MESSAGE constant

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -4,3 +4,5 @@ export const DEFAULT_PORT = 3000;
 export const ERROR_EMAIL_EXISTS = 'Email already registered';
 export const ERROR_INVALID_CREDENTIALS = 'Invalid credentials';
 export const ERROR_USER_NOT_FOUND = 'User not found';
+
+export const PASSWORD_MIN_MESSAGE = 'Password must be at least 6 characters';
