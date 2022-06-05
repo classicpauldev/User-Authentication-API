d0a88c66961125f4449beae40b32cd398b27835c
classicpauldev <1337@classicpaul.dev>
2022-06-05 09:00:00 +0100

    Add explicit return type to HealthController.check

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -4,7 +4,7 @@ import { Controller, Get } from '@nestjs/common';
 @Controller('health')
 export class HealthController {
   @Get()
-  check() {
+  check(): { status: string; timestamp: string } {
     return { status: 'ok', timestamp: new Date().toISOString() };
   }
 }
