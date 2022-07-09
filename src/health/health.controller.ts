e404ff70e07be4111716eba389e898fe622b18ba
classicpauldev <1337@classicpaul.dev>
2022-07-09 09:00:00 +0100

    Mark HealthController.check as readonly

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -4,7 +4,7 @@ import { Controller, Get } from '@nestjs/common';
 @Controller('health')
 export class HealthController {
   @Get()
-  check(): { status: string; timestamp: string } {
+  readonly check(): { status: string; timestamp: string } {
     return { status: 'ok', timestamp: new Date().toISOString() };
   }
 }
