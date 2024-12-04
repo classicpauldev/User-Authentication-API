b817f2037be6032d8ae11cd1f827b3aed7e758e9
classicpauldev <1337@classicpaul.dev>
2024-12-04 09:00:00 +0100

    Add ParseObjectIdPipe for MongoDB ID validation

---
+++ src/common/parse-objectid.pipe.ts
@@ -0,0 +1,11 @@
+import { PipeTransform, BadRequestException } from '@nestjs/common';
+import { Types } from 'mongoose';
+
+export class ParseObjectIdPipe implements PipeTransform<string, Types.ObjectId> {
+  transform(value: string): Types.ObjectId {
+    if (!Types.ObjectId.isValid(value)) {
+      throw new BadRequestException('Invalid ID format');
+    }
+    return new Types.ObjectId(value);
+  }
+}
