ef56194bd4c23ee9f81673b1fcddc40c6494c35f
classicpauldev <1337@classicpaul.dev>
2025-02-02 09:00:00 +0100

    Add JSDoc to configuration

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,3 +1,4 @@
+/** Application configuration from environment. */
 export default () => ({
   port: parseInt(process.env.PORT || '3000', 10),
   mongodbUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/user-auth-api',
