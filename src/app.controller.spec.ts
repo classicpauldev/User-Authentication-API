0dc17976ce0910a692035d264bea0993c62a3f42
classicpauldev <1337@classicpaul.dev>
2025-02-09 09:00:00 +0100

    Add test case 2

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,6 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('Test case 1', () => { expect(true).toBe(true); });
+    it('Test case 2', () => { expect(true).toBe(true); });
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
